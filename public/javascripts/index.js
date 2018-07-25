f7af417d2bc6d4d01f696a97a9e6a3cd45b2346e
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-07-25 22:06:02 +0200

    User session and user navbar

---
+++ public/javascripts/index.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+    hoverUser(); 
+    clickUser();  
+});
+
+function hoverUser(){
+    $( ".user-navbar" ).hover(
+        function() {
+          $( '.infoUser' ).show();
+        }, 
+        function() {
+          $( '.infoUser' ).hide();
+        }
+      );
+}
+function clickUser(){
+    $( ".user-navbar" ).on( "click", function() {
+        $( '.infoUser' ).hide();
+    });
+}
