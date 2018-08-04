9f7b60bbcbad59a28a168b7e32eb9c968d9d6132
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-08-04 17:54:38 +0200

    middelware

--- routes/login.js
+++ routes/login.js
@@ -8,13 +8,13 @@ const authMiddleware = require('../middlewares/auth.middleware');
 router.get('/', authMiddleware.userNotAuthenticated, loginController.render);
 router.get('/logout', loginController.delete);
 
-router.post('/spotify', authMiddleware.userNotAuthenticated, passport.authenticate('spotify-auth', {scope: [
+router.post('/spotify', passport.authenticate('spotify-auth', {scope: [
     'user-read-email', 
     'user-read-private',
     'user-read-recently-played',
     'user-top-read'
 ] }),);
-router.get('/:provider/cb', authMiddleware.userNotAuthenticated, loginController.createWithIDPCallback);
+router.get('/:provider/cb', loginController.createWithIDPCallback);
 
 
 module.exports = router;
