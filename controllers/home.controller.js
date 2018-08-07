3c249b62bd237b1bfa83c47b476a5d51e67fc18e
Alex González <alexgm@MacBook-Pro-de-Alex.local>
2018-08-07 19:49:23 +0200

    logs to debug

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -15,11 +15,13 @@ module.exports.postRender = (req, res, next) => {
             } ]).sort({createdAt:-1}).limit(1)
         ])
         .then((result)=>{
+            console.error('result promise: '+ result[1][0]); 
             let artistsIds = result[1][0].topArtists.reduce((acc,item)=>{
                 acc.push(item.artistId);
                 return acc;
-            },[]).slice(0,5)                     
-            serviceSpotify.getRecomendationsAndReleases(result[0].accessToken, result[0].refreshToken, artistsIds)
+            },[]).slice(0,5)   
+            console.error('artistsIds' + artistsIds);                  
+            serviceSpotify.getRecomeresultsndationsAndReleases(result[0].accessToken, result[0].refreshToken, artistsIds)
                 .then((results)=>{
                     res.render('home',{recomendations:results.recomendations.slice(0,6),releases:results.releases.slice(0,6)});
                 })      
