14d9a6735de4045608a838301c092ec37b0610b4
Alejandro Gonzalez <alejandro@MacBook-Pro-de-Alejandro.local>
2020-09-23 23:40:34 +0200

    Fix deprecated mongoose connection

--- configs/db.config.js
+++ configs/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.info(`Connected to the database: ${MONGODB_URI}`)
     })
