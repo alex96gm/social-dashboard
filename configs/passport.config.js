54befe06b1afd065e6da937da46d635d013f98a0
Alejandro Gonzalez <alejandro@MacBook-Pro-de-Alejandro.local>
2020-09-24 00:15:49 +0200

    Fix deprecation SpotifyStrategy passport

--- configs/passport.config.js
+++ configs/passport.config.js
@@ -21,7 +21,7 @@ module.exports.setup = (passport) => {
 
 
 
-    passport.use('spotify-auth', new SpotifyStrategy({
+    passport.use(new SpotifyStrategy({
         clientID: process.env.SPOTIFY_AUTH_CLIENT_ID || '',
         clientSecret: process.env.SPOTIFY_AUTH_CLIENT_SECRET || '',
         callbackURL: process.env.SPOTIFY_AUTH_CB || '/login/spotify/cb',
